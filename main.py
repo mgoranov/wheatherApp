9ae69d268674dc4c8e0c3d44e28a570aac37c109
MiroG <miroslav.goranov@gmail.com>
2014-06-12 21:43:32 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class WeatherApp(App):
+    pass
+
+
+class AddLocationForm(BoxLayout):
+    pass
+
+if __name__ == '__main__':
+    WeatherApp().run()
+    #AddLocationFormApp().run()
