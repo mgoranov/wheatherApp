a2e6cdd806d38b73ec3f9f01da9f41c1ea8de76b
MiroG <miroslav.goranov@gmail.com>
2014-06-14 21:48:50 +0300

    some error handling

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ class AddLocationForm(BoxLayout):
     def on_search(self):
         search_template = "http://api.openweathermap.org/data/2.5/find?q={}&type=like"
         search_url = search_template.format(self.tbSearchLocation.text)
-        request = UrlRequest(search_url, on_success=self.found_location, on_error=self.notfound_location)
+        UrlRequest(search_url, on_success=self.found_location, on_error=self.on_search_error)
 
-    def notfound_location(self, request, error):
+    def on_search_error(self, request, error):
         print(error)
 
     def found_location(self, request, data):
+        if 'cod' in data:
+            if data['cod'] == '404':
+                self.lstSearchResults.item_strings=['Nothing has been found!']
+                return
         cities = ["{} ({})".format(d['name'], d['sys']['country'])
                   for d in data['list']]
         self.lstSearchResults.item_strings = cities
