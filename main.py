03b6d449fe97f6fe0b650804fd88257b77e22533
MiroG <miroslav.goranov@gmail.com>
2014-06-14 20:58:02 +0300

    ........

--- main.py
+++ main.py
@@ -9,21 +9,23 @@ class WeatherApp(App):
 
 
 class AddLocationForm(BoxLayout):
-
     tbSearchLocation = ObjectProperty()
     lstSearchResults = ObjectProperty()
 
     def on_search(self):
         search_template = "http://api.openweathermap.org/data/2.5/find?q={}&type=like"
         search_url = search_template.format(self.tbSearchLocation.text)
-        request = UrlRequest(search_url, self.found_location)
+        request = UrlRequest(search_url, on_success=self.found_location, on_error=self.notfound_location)
 
-    def found_location(self,request,data):
-            cities = ["{} ({})".format(d['name'], d['sys']['country'])
-                for d in data['list']]
-            self.lstSearchResults.item_strings = cities
-            #print("\n".join(cities))
+    def notfound_location(self, request, error):
+        print(error)
+
+    def found_location(self, request, data):
+        cities = ["{} ({})".format(d['name'], d['sys']['country'])
+                  for d in data['list']]
+        self.lstSearchResults.item_strings = cities
+        # print("\n".join(cities))
+
 
 if __name__ == '__main__':
     WeatherApp().run()
-    #AddLocationFormApp().run()
